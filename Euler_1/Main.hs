757673e462e137403562173f6fcd429f37588a27
Golden <joagyl@student.chalmers.se>
2016-11-14 22:50:10 +0100

    Removed files

--- Euler_1/Main.hs
+++
@@ -1,7 +0,0 @@
-multiples :: Integer
-multiples = sum_list [ x | x <- [0..999], x `mod` 3 == 0 || x `mod` 5 == 0]
-		
-		
-sum_list :: [Integer] -> Integer
-sum_list []		= 0
-sum_list (x:xs) = x + sum_list(xs)
