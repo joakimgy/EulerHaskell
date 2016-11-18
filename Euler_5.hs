75b9fb8664f6bbc2fc8014d41d699c875a4b00ed
Golden <joagyl@student.chalmers.se>
2016-11-18 22:58:32 +0100

    Implemented sMul which find lcm of 1-20

--- Euler_5.hs
+++ Euler_5.hs
@@ -1,2 +1,8 @@
+--sMul :: Integer
+--sMul = head [ x | x <- [2..], check x ]
+
+--check :: Integer -> Bool
+--check n = all (==0) [n `mod` x | x <- [2..19] ]
+
 sMul :: Integer
-sMul = head [x | x<-[40,60..], all (\y -> x`mod`y == 0) [2..20] ]
+sMul = foldr1 lcm [1..20]
